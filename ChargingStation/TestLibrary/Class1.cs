6c1addb05fd439a9c0b0e396afb6e0683b74f479
Cyber_Kuryber <urosstanic25@gmail.com>
2022-06-29 15:44:53 +0200

    it aint working

---
+++ ChargingStation/TestLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using System.Dynamic;
+using ChargingStation.Service.RealtimeReport;
+
+namespace TestLibrary
+{
+}
---
+++ ChargingStation/TestLibrary/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using System.Dynamic;
+using ChargingStation.Service.RealtimeReport;
+
+namespace TestLibrary
+{
+    public class Tests
+    {
+        [Fact]
+        public void HubsAreMockableViaDynamic()
+        {
+            bool sendCalled = false;
+            var hub = new MainHub();
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            hub.Clients = (Microsoft.AspNetCore.SignalR.IHubCallerClients)mockClients.Object;
+            dynamic all = new ExpandoObject();
+            all.broadcastMessage = new Action<string, string>((name, message) =>
+            {
+                sendCalled = true;
+            });
+            mockClients.Setup(m => m.All).Returns((ExpandoObject)all);
+            hub.Send(1, 1);
+            Assert.True(sendCalled);
+        }
+    }
+}
